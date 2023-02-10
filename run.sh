04c28a99d96426d3efb2c2af76112b37536c9c14
jbsouthe <jbsouthe@github.com>
2023-02-10 12:03:03 -0700

    added run wrapper

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+if[ ! -f ./JavaAgent/javaagent.jar ]; then
+    echo "./JavaAgent/javaagent.jar not found!"
+    exit
+fi
+
+JAVAAGENT="-javaagent:./JavaAgent/javaagent.jar"
+
+java $JAVAAGENT -jar $1 
